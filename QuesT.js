983f15f7a83428f819075d05e7ef4f100e488a11
ARNAV-MODGIL <240049496+ArnavModgiL@users.noreply.github.com>
2026-04-28 15:43:05 +0530

    Refactor and correct logic for basic programming tasks

--- QuesT.js
+++ QuesT.js
@@ -97,3 +97,16 @@ if(c > largest){
     largest = c;
 }
 console.log("Largest Number",largest);
+
+
+// Fibonacci Series using prompt()
+
+let n = prompt("enter the number");
+let a = 0;
+let b = 1;
+for(let i = 1; i <= n ; i++) {
+    console.log(a);
+    let temp = a + b;
+    a = b;
+    b = temp;
+}
